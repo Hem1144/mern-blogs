c17e78cc6fea2d0eb237f5b5264caae49f269499
Hem1144 <hemlaldulal7@gmail.com>
2024-06-01 23:45:40 +0545

    feat: Maintain server.ts file according to needed

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,24 +1,19 @@
-import express from "express";
 import "dotenv/config";
+import express from "express";
 import mongoose from "mongoose";
-import env from "./util//validateEnv";
+import env from "./util/validateEnv";
 
 const app = express();
-const port = env.PORT;
 
+const port = env.PORT;
 const mongoConnectionString = env.MONGO_CONNECTION_STRING;
-if (!mongoConnectionString) {
-  throw new Error(
-    "MONGO_CONNECTION_STRING environment variable is not defined"
-  );
-}
 
 app.get("/", (req, res) => {
   res.send("This is get API !");
 });
 
 mongoose
-  .connect(mongoConnectionString)
+  .connect(mongoConnectionString as string)
   .then(() => {
     console.log("Connected to MongoDB");
     app.listen(port, () => {
@@ -27,4 +22,7 @@ mongoose
   })
   .catch((err) => {
     console.error("Failed to connect to MongoDB", err);
+    process.exit(1);
   });
+
+export default app;
