b1afc6d524e0279167b18434d8a226dd146db766
Hem1144 <hemlaldulal7@gmail.com>
2024-06-01 23:17:55 +0545

    feat: Connect to mongoDB databse

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,29 @@
 import express from "express";
+import "dotenv/config";
+import mongoose from "mongoose";
 
 const app = express();
-const port = undefined;
+const port = process.env.PORT;
+
+const mongoConnectionString = process.env.MONGO_CONNECTION_STRING;
+if (!mongoConnectionString) {
+  throw new Error(
+    "MONGO_CONNECTION_STRING environment variable is not defined"
+  );
+}
 
 app.get("/", (req, res) => {
   res.send("This is get API !");
 });
 
-app.listen(port!, () => {
-  console.log(`Server is running on port ${port}`);
-});
+mongoose
+  .connect(mongoConnectionString)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB", err);
+  });
