cf24b2ac249336b11e588cea730b15fecddd7020
Hem1144 <hemlaldulal7@gmail.com>
2024-06-02 20:41:43 +0545

    feat: Added morgan library in a project

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,14 @@
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
 import notesRoutes from "./routes/notes";
+import morgan from "morgan";
+
 const app = express();
 
+app.use(morgan("dev"));
+
 app.use(express.json());
+
 app.use("/api/notes", notesRoutes);
 
 app.use((req, res, next) => {
