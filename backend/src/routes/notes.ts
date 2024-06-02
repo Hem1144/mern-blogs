38e37998d9c396e5a33518301ca1475b64ff7e5f
Hem1144 <hemlaldulal7@gmail.com>
2024-06-02 18:43:24 +0545

    feat: Added routes for create note and getNoteById

--- backend/src/routes/notes.ts
+++ backend/src/routes/notes.ts
@@ -4,5 +4,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", NoteController.getNotes);
+router.get("/:noteId", NoteController.getNote);
+router.post("/", NoteController.createNote);
 
 export default router;
