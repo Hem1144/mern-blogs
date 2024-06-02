ff8e409ff938b387924885aadc5a2c62054fcd1c
Hem1144 <hemlaldulal7@gmail.com>
2024-06-02 21:24:11 +0545

    feat: Handled HTTP error using http-error

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,24 +2,28 @@ import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
 import notesRoutes from "./routes/notes";
 import morgan from "morgan";
+import createHttpError, { isHttpError } from "http-errors";
 
 const app = express();
 
 app.use(morgan("dev"));
-
 app.use(express.json());
 
 app.use("/api/notes", notesRoutes);
 
 app.use((req, res, next) => {
-  next(Error("Endpoint not found!"));
+  next(createHttpError(404, "Endpoint not found!"));
 });
 
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
   console.error(error);
-  let errorMessage = "An unknown error occured";
-  if (error instanceof Error) errorMessage = error.message;
-  res.status(500).json({ message: errorMessage });
+  let errorMessage = "An unknown error occurred";
+  let statusCode = 500;
+  if (isHttpError(error)) {
+    statusCode = error.status;
+    errorMessage = error.message;
+  }
+  res.status(statusCode).json({ message: errorMessage });
 });
 
 export default app;
