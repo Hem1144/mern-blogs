e6c631bb0148d267b8f780ac7ae1f0e850f41645
Hem1144 <hemlaldulal7@gmail.com>
2024-06-02 22:45:34 +0545

    feat: Implemented API logic for update and delete notes

--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -2,6 +2,7 @@ import { RequestHandler } from "express";
 import NoteModel from "../models/note";
 import createHttpError from "http-errors";
 import mongoose from "mongoose";
+import note from "../models/note";
 
 export const getNotes: RequestHandler = async (req, res, next) => {
   try {
@@ -54,6 +55,70 @@ export const createNote: RequestHandler<
     });
 
     res.status(201).json(newNote);
+  } catch (error) {
+    next(error);
+  }
+};
+
+interface UpdateNoteParams {
+  noteId: string;
+}
+interface UpdateNoteBody {
+  title?: string;
+  text?: string;
+}
+
+export const updateNote: RequestHandler<
+  UpdateNoteParams,
+  unknown,
+  UpdateNoteBody,
+  undefined
+> = async (req, res, next) => {
+  const noteId = req.params.noteId;
+  const newTitle = req.body.title;
+  const newText = req.body.text;
+  try {
+    if (!mongoose.isValidObjectId(noteId)) {
+      throw createHttpError(400, "Note Id invalid");
+    }
+
+    if (!newTitle) {
+      throw createHttpError(400, "Note must have a title");
+    }
+
+    const note = await NoteModel.findById(noteId).exec();
+
+    if (!note) {
+      throw createHttpError(404, "Note not found with provided Id");
+    }
+    note.title = newTitle;
+    note.text = newText;
+
+    const updatedNote = await note.save();
+    res.status(200).json(updatedNote);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteNote: RequestHandler = async (req, res, next) => {
+  const noteId = req.params.noteId;
+
+  try {
+    if (!mongoose.isValidObjectId(noteId)) {
+      throw createHttpError(400, "Note Id invalid");
+    }
+
+    const note = await NoteModel.findById(noteId).exec();
+
+    if (!note) {
+      throw createHttpError(404, "Note not found with provided Id");
+    }
+
+    await NoteModel.deleteOne({ _id: noteId });
+    // Alternatively, you can use: await NoteModel.findByIdAndDelete(noteId);
+
+    res.sendStatus(204);
   } catch (error) {
     next(error);
   }
--- backend/src/routes/notes.ts
+++ backend/src/routes/notes.ts
@@ -6,5 +6,7 @@ const router = express.Router();
 router.get("/", NoteController.getNotes);
 router.get("/:noteId", NoteController.getNote);
 router.post("/", NoteController.createNote);
+router.patch("/:noteId", NoteController.updateNote);
+router.delete("/:noteId", NoteController.deleteNote);
 
 export default router;
