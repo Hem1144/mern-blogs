98f2fb29e0b46ca4291659c036f135da33f4e8b8
Hem1144 <hemlaldulal7@gmail.com>
2024-06-03 23:08:46 +0545

    feat: Got notes from backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import { Button } from "react-bootstrap";
 import { Note } from "./models/note";
+import axios from "axios";
 
 function App() {
   const [notes, setNotes] = useState<Note[]>([]);
@@ -9,10 +10,13 @@ function App() {
   useEffect(() => {
     async function loadNotes() {
       try {
-        const reponse = await fetch("/api/notes", {
-          method: "GET",
-        });
-        const notes = await reponse.json();
+        //! alternative way to get notes
+        // const reponse = await fetch("/api/notes", {
+        //   method: "GET",
+        // });
+        // const notes = await reponse.json();
+
+        const notes = (await axios.get("/api/notes")).data;
         setNotes(notes);
       } catch (error) {
         console.error(error);
