ade665636fe9d66bf4e057ed2f8da7ecb963e2fd
Hem1144 <hemlaldulal7@gmail.com>
2024-06-09 23:54:54 +0545

    feat: Handled form data using react-hook-form Hook

--- client/src/components/AddNoteDialog.tsx
+++ client/src/components/AddNoteDialog.tsx
@@ -1,16 +1,67 @@
-import { Form, Modal } from "react-bootstrap";
+import { Button, Form, Modal } from "react-bootstrap";
+import { Note } from "../models/note";
+import { useForm } from "react-hook-form";
+import { NoteInput } from "../network/notes_api";
+import * as NotesApi from "../network/notes_api";
 
 interface AddNoteDialogProps {
   onDismiss: () => void;
+  onNoteSaved: (note: Note) => void;
 }
 
-const AddNoteDialog = ({ onDismiss }: AddNoteDialogProps) => {
+const AddNoteDialog = ({ onDismiss, onNoteSaved }: AddNoteDialogProps) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<NoteInput>();
+
+  async function onSubmit(input: NoteInput) {
+    try {
+      const noteResponse = await NotesApi.createNote(input);
+      onNoteSaved(noteResponse);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <Modal show onHide={onDismiss}>
       <Modal.Header closeButton>
         <Modal.Title>Add Note</Modal.Title>
       </Modal.Header>
-      <Form> </Form>
+      <Modal.Body>
+        <Form id="addNoteForm" onSubmit={handleSubmit(onSubmit)}>
+          <Form.Group className="mb-3">
+            <Form.Label>Title</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Title"
+              isInvalid={!!errors.title}
+              {...register("title", { required: "Required" })}
+            />
+            <Form.Control.Feedback type="invalid">
+              {errors.title?.message}
+            </Form.Control.Feedback>
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label>Text</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={5}
+              placeholder="Enter your text"
+              {...register("text")}
+            />
+          </Form.Group>
+        </Form>
+      </Modal.Body>
+
+      <Modal.Footer>
+        <Button type="submit" form="addNoteForm">
+          Save
+        </Button>
+      </Modal.Footer>
     </Modal>
   );
 };
